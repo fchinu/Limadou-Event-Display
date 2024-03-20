aa98eefeecd0b56c5e20e50993bdf2be44967c1b
Bizzzio <fachi2001@gmail.com>
2024-03-20 16:48:56 +0100

    Fix Format

--- Clustering/include/cluster.h
+++ Clustering/include/cluster.h
@@ -1,15 +1,10 @@
-#ifndef CLUSTER_H
-#define CLUSTER_H
+#ifndef CLUSTERING_INCLUDE_CLUSTER_H_
+#define CLUSTERING_INCLUDE_CLUSTER_H_
 
-
-
-class Cluster
-{
-    int fX;   
-    int fY; 
-    int ClusterSize;
-        
-
+class Cluster {
+  int fX;
+  int fY;
+  int ClusterSize;
 };
 
-#endif
+#endif // CLUSTERING_INCLUDE_CLUSTER_H_
