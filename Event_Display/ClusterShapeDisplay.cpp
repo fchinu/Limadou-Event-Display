7a759ef75eba8965a04a6bd1d566ebe1dbcb29a7
Roberta-Ferioli <roberta.ferioli@edu.unito.it>
2024-03-21 22:13:18 +0100

    fix format

--- Event_Display/ClusterShapeDisplay.cpp
+++ Event_Display/ClusterShapeDisplay.cpp
@@ -1,10 +1,10 @@
 #include "TCanvas.h"
 #include "TH2D.h"
 #include "TStyle.h"
+#include <TGraphErrors.h>
+#include <TMarker.h>
 #include <bitset>
 #include <iostream>
-#include <TMarker.h>
-#include <TGraphErrors.h>
 
 void fillBinaryMatrix(TH2D *histCluster, const uint8_t *ExampleArray, int size,
                       int max_length, int xClusterPosition,
@@ -13,36 +13,35 @@ void fillBinaryMatrix(TH2D *histCluster, const uint8_t *ExampleArray, int size,
     std::bitset<8> bits(ExampleArray[i]);
     for (int j = 0; j < max_length; j++) {
       histCluster->Fill(xClusterPosition + j, yClusterPosition - i - 1,
-                 bits[max_length - 1 - j]); // Adjusting the y-position
+                        bits[max_length - 1 - j]); // Adjusting the y-position
     }
   }
 }
 
 void ClusterShapeDisplay() {
   // need to link t cluster class
-  uint8_t ExampleArray[] = {16, 48, 48, 224, 32, 0, 0, 0};// Example array
-  //uint8_t ExampleArray[] = {192, 192, 80, 248, 16, 16, 16, 16};
+  uint8_t ExampleArray[] = {16, 48, 48, 224, 32, 0, 0, 0}; // Example array
+  // uint8_t ExampleArray[] = {192, 192, 80, 248, 16, 16, 16, 16};
   int size = sizeof(ExampleArray) / sizeof(ExampleArray[0]);
   int max_length = 8;
   int xClusterPosition = 123;
   int yClusterPosition = 456;
-  //int xClusterPosition = 379;
-  //int yClusterPosition = 28;
-  double xMean=125; 
-  double yMean=453.5;
-  double xErrorBar=2;
-  double yErrorBar=2.5;
-  //double xMean=381.5; //to be linked to the cluster class, made with mean of random numbers used before
-  //double yMean=24.0;
-  //double xErrorBar=2.5;
-  //double yErrorBar=4.0;
+  // int xClusterPosition = 379;
+  // int yClusterPosition = 28;
+  double xMean = 125;
+  double yMean = 453.5;
+  double xErrorBar = 2;
+  double yErrorBar = 2.5;
+  // double xMean=381.5; //to be linked to the cluster class, made with mean of
+  // random numbers used before double yMean=24.0; double xErrorBar=2.5; double
+  // yErrorBar=4.0;
 
-  TH2D *histCluster = new TH2D("Cluster", "Cluster", max_length, xClusterPosition,
-                        xClusterPosition + max_length, size,
-                        yClusterPosition - size, yClusterPosition);
+  TH2D *histCluster = new TH2D("Cluster", "Cluster", max_length,
+                               xClusterPosition, xClusterPosition + max_length,
+                               size, yClusterPosition - size, yClusterPosition);
 
-  fillBinaryMatrix(histCluster, ExampleArray, size, max_length, xClusterPosition,
-                   yClusterPosition);
+  fillBinaryMatrix(histCluster, ExampleArray, size, max_length,
+                   xClusterPosition, yClusterPosition);
 
   TCanvas *canvas = new TCanvas("canvas", "Canvas", 1000, 1000);
   gStyle->SetOptStat(0);
@@ -51,14 +50,14 @@ void ClusterShapeDisplay() {
   histCluster->GetXaxis()->SetTitle("X (pixel)");
   histCluster->GetYaxis()->SetTitle("Y (pixel)");
   TGraphErrors *PointGraph = new TGraphErrors();
-  PointGraph->SetPoint(0, xMean, yMean); 
+  PointGraph->SetPoint(0, xMean, yMean);
   PointGraph->SetPointError(0, xErrorBar, yErrorBar);
   PointGraph->SetLineColor(kRed);
   PointGraph->SetLineWidth(3);
-  PointGraph->SetMarkerStyle(20); 
-  PointGraph->SetMarkerColor(kRed); 
+  PointGraph->SetMarkerStyle(20);
+  PointGraph->SetMarkerColor(kRed);
   PointGraph->SetMarkerSize(2);
-  PointGraph->Draw("P,same"); 
+  PointGraph->Draw("P,same");
 
   canvas->Update();
   canvas->SaveAs("clusterShape1.png");
