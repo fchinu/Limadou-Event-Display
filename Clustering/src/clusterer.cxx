00ad24be884d77a91dc8e6481d1a5edf16ad485a
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2024-03-22 08:38:12 +0100

    clusterer.cxx now fully implements the clustering algorithm

--- Clustering/src/clusterer.cxx
+++ Clustering/src/clusterer.cxx
@@ -1,90 +1,100 @@
 
+#include <algorithm>
+#include <array>
+#include <bitset>
 #include <iostream>
 #include <unordered_map>
-#include <algorithm>
 
-#include "clusterer.h"
 #include "../../Utils/include/fileManager.h"
+#include "clusterer.h"
 
-Clusterer::Clusterer()
-{
-}
+Clusterer::Clusterer() {}
 
-Clusterer::~Clusterer()
-{
-}
+Clusterer::~Clusterer() {}
 
-void Clusterer::Clustering(const char * inputFile)
-{
+void Clusterer::Clustering(const char *inputFile) {
 
-    // add filemanager 
-    FileManager inputData(inputFile);
+  // add filemanager
+  FileManager inputData(inputFile);
 
-    std::unordered_map<int, std::vector<int>> event_indices;
+  std::unordered_map<int, std::vector<int>> eventIndices;
 
-    double *event = inputData.getColumn("event_count");
-    double *chip_id = inputData.getColumn("chip_id(decimal)");
-    double *x = inputData.getColumn("hit_x");
-    double *y = inputData.getColumn("hit_y");
+  double *event = inputData.getColumn("event_count");
+  double *chipID = inputData.getColumn("chip_id(decimal)");
+  double *x = inputData.getColumn("hit_x");
+  double *y = inputData.getColumn("hit_y");
 
-    for (int irow = 0; irow < inputData.getNRows(); irow++)
-    {
-        event_indices[event[irow]].push_back(irow);
-    }
+  for (int irow = 0; irow < inputData.getNRows(); irow++) {
+    eventIndices[event[irow]].push_back(irow);
+  }
 
-    for (auto it = event_indices.begin(); it != event_indices.end(); ++it)
-    {
-        int ievent = it->first;
-        std::vector<int>& indices = it->second;
+  for (auto it = eventIndices.begin(); it != eventIndices.end(); ++it) {
+    int ievent = it->first;
+    std::vector<int> &indices = it->second;
 
-        std::cout << "Event " << ievent << std::endl;
-        std::cout << "Indices size: " << indices.size() << std::endl;
+    std::cout << "Event " << ievent << std::endl;
+    std::cout << "Indices size: " << indices.size() << std::endl;
 
-        while (!indices.empty())
-        {
+    while (!indices.empty()) {
 
-            std::vector<int> next_indices;
-            double mean_ix = x[indices[0]], mean_iy = y[indices[0]];
-            int cluster_size = 1;
+      std::vector<int> nextIndices;
+      nextIndices.push_back(indices[0]);
+      double meanX = x[indices[0]], meanY = y[indices[0]];
+      int clusterSize = 1;
 
-            for (int i = 1; i < indices.size(); i++)
-            {
-                int index = indices[i];
+      for (int i = 1; i < indices.size(); i++) {
+        int index = indices[i];
 
-                if (chip_id[index] != chip_id[indices[0]]) continue;
-                
-                bool isValid = false;
-                if ((std::abs(x[index] - x[indices[0]]) + std::abs(y[index] - y[indices[0]])) <= 1) isValid = true;
-                for (int j : next_indices)
-                {
-                    if (std::abs(x[index] - x[j]) == 1 && std::abs(y[index] - y[j]) == 1)   isValid = true;
-                }
+        if (chipID[index] != chipID[indices[0]])
+          continue;
 
-                if (isValid)
-                {
-                    next_indices.push_back(index);
-                    mean_ix += x[index];
-                    mean_iy += y[index];
-                    cluster_size++;
-                }
-            }
-
-            mean_ix /= cluster_size;
-            mean_iy /= cluster_size;
-
-            // cluster class implementation needed
-            Cluster cluster( static_cast<int>(chip_id[indices[0]]), 
-                             static_cast<int>(ievent), 
-                             mean_ix, 
-                             mean_iy, 
-                             cluster_size);
-            fClusters.push_back(cluster);
-
-            // Remove processed indices from the vector
-            indices.erase(std::remove_if(indices.begin(), indices.end(),
-                [&next_indices](int i) { return std::find(next_indices.begin(), next_indices.end(), i) != next_indices.end(); }),
-                indices.end());
-            indices.erase(indices.begin());
+        bool isValid = false;
+        for (int j : nextIndices) {
+          if (std::abs(x[index] - x[j]) == 1 + std::abs(y[index] - y[j]) <= 1)
+            isValid = true;
         }
+
+        if (isValid) {
+          nextIndices.push_back(index);
+          meanX += x[index];
+          meanY += y[index];
+          clusterSize++;
+        }
+      }
+
+      size_t size = nextIndices.size();
+      int xPos[size], yPos[size];
+      for (int i = 0; i < size; i++) {
+        xPos[i] = int(x[nextIndices[i]]);
+        yPos[i] = int(y[nextIndices[i]]);
+      }
+
+      int minX = *std::min_element(xPos, xPos + size);
+      int minY = *std::min_element(yPos, yPos + size);
+
+      meanX /= clusterSize;
+      meanY /= clusterSize;
+
+      // cluster shape
+      std::array<std::bitset<MAX_CLUSTER_COLS>, MAX_CLUSTER_ROWS> shape = {0};
+      for (int i = 0; i < size; i++)
+        shape[yPos[i] - minY].set(xPos[i] - minX);
+
+      // cluster class implementation needed
+      Cluster cluster(static_cast<unsigned>(chipID[indices[0]]),
+                      static_cast<unsigned>(ievent), minX, minY, shape, meanX,
+                      meanY, clusterSize);
+      fClusters.push_back(cluster);
+
+      // Remove processed indices from the vector
+      indices.erase(std::remove_if(indices.begin(), indices.end(),
+                                   [&nextIndices](int i) {
+                                     return std::find(nextIndices.begin(),
+                                                      nextIndices.end(),
+                                                      i) != nextIndices.end();
+                                   }),
+                    indices.end());
+      indices.erase(indices.begin());
     }
+  }
 }
