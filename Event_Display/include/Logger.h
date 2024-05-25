6450a6c262a5eb4f763537365c84239081276a22
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2024-05-25 12:47:26 +0200

    clang-format

--- Event_Display/include/Logger.h
+++ Event_Display/include/Logger.h
@@ -1,8 +1,15 @@
-#pragma once 
+#pragma once
 
-namespace limadou
-{
-    #define LOG_DEBUG(...) do { std::printf(__VA_ARGS__); std::printf("\n"); } while(0)
+namespace limadou {
+#define LOG_DEBUG(...)                                                         \
+  do {                                                                         \
+    std::printf(__VA_ARGS__);                                                  \
+    std::printf("\n");                                                         \
+  } while (0)
 
-    #define LOG_ERROR(...) do { std::fprintf(stderr, __VA_ARGS__); std::fprintf(stderr, "\n"); } while(0)
-}
+#define LOG_ERROR(...)                                                         \
+  do {                                                                         \
+    std::fprintf(stderr, __VA_ARGS__);                                         \
+    std::fprintf(stderr, "\n");                                                \
+  } while (0)
+} // namespace limadou
